56746639073218d47c1c371d284aa68c2143f348
heratta <mr.heratta@gmail.com>
2021-06-04 08:53:47 +0900

    不要なコードの削除

--- Assets/Helab/Scripts/Simply/Movement/SimpleKineticEnergy.cs
+++ Assets/Helab/Scripts/Simply/Movement/SimpleKineticEnergy.cs
@@ -2,19 +2,11 @@ using UnityEngine;
 
 namespace Helab.Simply.Movement
 {
-    [RequireComponent(typeof(SimpleMovement))]
     public abstract class SimpleKineticEnergy : MonoBehaviour
     {
         public bool IsEnabledUpdate { get; set; } = true;
         
         public Vector3 DeltaMovement { get; protected set; }
-
-        protected SimpleMovement Movement { get; private set; }
-
-        private void Start()
-        {
-            Movement = GetComponent<SimpleMovement>();
-        }
 
         private void Update()
         {
